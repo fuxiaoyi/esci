5d923e61570e691c458429230163190d2f56e6aa
TensorBeats <xiaoyif32@yahoo.com>
2025-09-18 18:06:16 +0800

    exclude settings and agent

--- next.config.mjs
+++ next.config.mjs
@@ -41,7 +41,7 @@ const config = {
       return {
           beforeFiles: [
               {
-                  source: '/((?!api|_next|_static|favicon.ico|zh|zhtw|en|hu|fr|de|it|ja|lt|ko|pl|pt|ro|ru|uk|es|nl|sk|hr|tr|signin).*)',
+                  source: '/((?!api|_next|_static|favicon.ico|zh|zhtw|en|hu|fr|de|it|ja|lt|ko|pl|pt|ro|ru|uk|es|nl|sk|hr|tr|signin|settings|agent).*)',
                   has: [
                       {
                           type: 'host',
